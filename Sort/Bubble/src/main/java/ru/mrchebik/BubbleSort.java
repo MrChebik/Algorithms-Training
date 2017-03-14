919ad47b50f3baf5bb713b6205bfd402ca8fa838
mrchebik <mrchebik@yandex.ru>
2017-03-14 15:05:34 +0200

    slightly improved Bubble sort

--- Sort/Bubble/src/main/java/ru/mrchebik/BubbleSort.java
+++ Sort/Bubble/src/main/java/ru/mrchebik/BubbleSort.java
@@ -13,10 +13,12 @@ public class BubbleSort {
     }
 
     public static int[] bubble(int[] array) {
-        for (int k = array.length - 1; k > 1; k--) {
-            for (int i = 0; i < k; i++) {
+        int temp, k, i;
+
+        for (k = array.length - 1; k > 1; k--) {
+            for (i = 0; i < k; i++) {
                 if (array[i] > array[i + 1]) {
-                    int temp = array[i + 1];
+                    temp = array[i + 1];
                     array[i + 1] = array[i];
                     array[i] = temp;
                 }
