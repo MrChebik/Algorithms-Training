5b739ba83a1f865ebec4aeda95b8161c8e0222cc
mrchebik <mrchebik@yandex.ru>
2017-03-18 17:43:51 +0200

    added example to Stack Structure

--- Structure/Stack/src/main/java/ru/mrchebik/Main.java
+++ Structure/Stack/src/main/java/ru/mrchebik/Main.java
@@ -16,5 +16,49 @@ public class Main {
         while (!stack.isEmpty()) {
             System.out.print(stack.pop() + " ");
         }
+        System.out.println();
+
+        brackets("a{b[c)d}e");
+    }
+
+    /**
+     * Example of using Stack,
+     * The method will look for matching parentheses
+     * @param str String with brackets.
+     */
+    public static void brackets(String str) {
+        Stack stackCharacters = new Stack(str.length());
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            switch(ch) {
+                case '{':
+                case '[':
+                case '(':
+                    stackCharacters.push(ch);
+                    break;
+
+                case '}':
+                case ']':
+                case ')':
+                    if (!stackCharacters.isEmpty()) {
+                        char chx = (char) stackCharacters.pop();
+                        if (ch == '}' && chx != '{' ||
+                                ch == ']' && chx != '[' ||
+                                ch == ')' && chx != '(') {
+                            System.out.println("Error " + ch + " at " + i);
+                        }
+                    } else {
+                        System.out.println("Error " + ch + " at " + i);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        if (!stackCharacters.isEmpty()) {
+            System.out.println("Error: missing right delimiter");
+        }
     }
 }
