290e323e0a918e795cf2f0f35a04ffa033272ba5
mrchebik <mrchebik@yandex.ru>
2017-03-20 17:15:01 +0200

    added new Example of program with Stack Structure -- 'Postfix Entry'

--- Structure/Stack/src/main/java/ru/mrchebik/Main.java
+++ Structure/Stack/src/main/java/ru/mrchebik/Main.java
@@ -19,6 +19,9 @@ public class Main {
         System.out.println();
 
         brackets("a{b[c)d}e");
+
+        System.out.println(trans("A*(B+C)-D/(E+F)"));
+        System.out.println(doParse("345+*612+/-"));
     }
 
     /**
@@ -60,5 +63,119 @@ public class Main {
         if (!stackCharacters.isEmpty()) {
             System.out.println("Error: missing right delimiter");
         }
+    }
+
+    /**
+     * Example of using Stack.
+     * The method will produce condition from infix to postfix entry.
+     * @param condition Condition of infix entry.
+     */
+    private static Stack opStack;
+    private static String output;
+
+    public static String trans(String condition) {
+        opStack = new Stack(condition.length());
+        output = "";
+
+        for (int j = 0; j < condition.length(); j++) {
+            char ch = condition.charAt(j);
+            switch (ch) {
+                case '+':
+                case '-':
+                    gotOp(ch, 1);
+                    break;
+                case '*':
+                case '/':
+                    gotOp(ch, 2);
+                    break;
+                case '(':
+                    opStack.push(ch);
+                    break;
+                case ')':
+                    gotParen(ch);
+                    break;
+                default:
+                    output += ch;
+                    break;
+            }
+        }
+
+        while (!opStack.isEmpty()) {
+            output += (char) opStack.pop();
+        }
+
+        return output;
+    }
+
+    private static void gotOp(char opThis, int prec1) {
+        while (!opStack.isEmpty()) {
+            char opTop = (char) opStack.pop();
+            if (opTop == '(') {
+                opStack.push(opTop);
+                break;
+            } else {
+                int prec2;
+                prec2 = opTop == '+' || opTop == '-' ? 1 : 2;
+                if (prec2 < prec1) {
+                    opStack.push(opTop);
+                    break;
+                } else {
+                    output += opTop;
+                }
+            }
+        }
+        opStack.push(opThis);
+    }
+
+    private static void gotParen(char ch) {
+        while (!opStack.isEmpty()) {
+            char chx = (char) opStack.pop();
+            if (chx == '(') {
+                break;
+            } else {
+                output += chx;
+            }
+        }
+    }
+
+    // Calculation of postfix condition
+
+    private static Stack parse;
+
+    public static int doParse(String postfix) {
+        parse = new Stack(postfix.length());
+        char ch;
+        int j;
+        int num1, num2, interAns;
+
+        for (j = 0; j < postfix.length(); j++) {
+            ch = postfix.charAt(j);
+            if (ch >= '0' && ch <= '9') {
+                parse.push((int) (ch - '0'));
+            } else {
+                num2 = (int) parse.pop();
+                num1 = (int) parse.pop();
+                switch (ch) {
+                    case '+':
+                        interAns = num1 + num2;
+                        break;
+                    case '-':
+                        interAns = num1 - num2;
+                        break;
+                    case '*':
+                        interAns = num1 * num2;
+                        break;
+                    case '/':
+                        interAns = num1 / num2;
+                        break;
+                    default:
+                        interAns = 0;
+                }
+                parse.push(interAns);
+            }
+        }
+        interAns = (int) parse.pop();
+
+        return interAns;
     }
 }
