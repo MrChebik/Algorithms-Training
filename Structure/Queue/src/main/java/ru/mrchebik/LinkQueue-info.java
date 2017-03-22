a8e4ad238c6e6aec8a39e451bd0e218bbaf3cb39
mrchebik <mrchebik@yandex.ru>
2017-03-22 21:16:46 +0200

    added info, about similar structures

---
+++ Structure/Queue/src/main/java/ru/mrchebik/LinkQueue-info.java
@@ -0,0 +1,6 @@
+/**
+ * In the Link module, you can find the implementation of the Queue Structure on the basis of Link Structure.
+ *
+ * Created by mrchebik on 22.03.17.
+ */
+package ru.mrchebik;
