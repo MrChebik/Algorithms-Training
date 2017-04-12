6e23fd43501065c88971734222af9d98421eba17
mrchebik <mrchebik@yandex.ru>
2017-04-12 12:24:40 +0300

    added Bitwise Sort

---
+++ Sort/Bitwise/src/main/java/Main.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by mrchebik on 12.04.17.
+ */
+public class Main {
+    private static Queue[] numbers = new Queue[10];
+    private static String[] standartNumbers = {"421", "240", "035", "532", "305", "430", "124"};
+
+    public static void main(String[] args) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = new LinkedList();
+        }
+
+        System.out.println(Arrays.toString(standartNumbers));
+        bitwise(standartNumbers[0].length() - 1);
+        System.out.println(Arrays.toString(standartNumbers));
+    }
+
+    public static void bitwise(int col) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < standartNumbers.length; j++) {
+                if (standartNumbers[j].charAt(col) == i + 48) {
+                    numbers[i].add(standartNumbers[j]);
+                }
+            }
+        }
+
+        int i = 0;
+
+        for (int j = 0; j < 10; j++) {
+            while (!numbers[j].isEmpty()) {
+                standartNumbers[i++] = String.valueOf(numbers[j].remove());
+            }
+        }
+
+        if (col != 0) {
+            bitwise(col - 1);
+        }
+    }
+}
